6ec99122d20d50c6359cfc6df08a8c20f6530846
Marcos Sanmartín <ahriman89@gmail.com>
2023-05-29 21:39:33 +0200

    README

--- claves.inc.php
+++ claves.inc.php
@@ -7,12 +7,12 @@
 // https://www.bingmapsportal.com/
 // https://console.cloud.google.com/?hl=es
 
-$keyAEMET = "";
+$keyAEMET = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhaHJpbWFuODlAZ21haWwuY29tIiwianRpIjoiMjM2YmNmY2MtNTRiYS00ZDMzLTlkNjQtMDE4ODFjNzkwODdiIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2ODQ0MzU5MzgsInVzZXJJZCI6IjIzNmJjZmNjLTU0YmEtNGQzMy05ZDY0LTAxODgxYzc5MDg3YiIsInJvbGUiOiIifQ.m2oTkOxE0bXMtdqo_y8NKGcUGf2G0fF6hzWs1tMfb1Q";
 
-$keyOpenWeatherMap  = '';  
+$keyOpenWeatherMap  = '26eb3631c14bd976d9387e8b50b2c99b';  
 
-$keyBing = "";  
+$keyBing = "AvIni4DG8XfB_jZwmG6B6W0fZhwX96qSo0-_jIEWaV91jnMlYgGYFVA6K3ee9uj8";  
 
-$googleClientId     = ''; 
-$googleClientSecret = ''; 
+$googleClientId     = '784523481714-l3h8ofpl3602a3917l4dlpcre9f2aq91.apps.googleusercontent.com'; 
+$googleClientSecret = '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'; 
 
